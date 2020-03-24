2a8cde25cbac67b315b309bdc4b6e76bbf9dea99
kuro-tsuchi <kurotsuchi33@gmail.com>
2020-03-24 10:22:01 +0800

    认证授权

--- golang/src/test/main.go
+++ golang/src/test/main.go
@@ -1,21 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	rdb *redis.ClusterClient
+)
+var logger = logrus.New()
+var entry = logger.WithFields(logrus.Fields{
+	"animal": "walrus",
+	"size":   10,
+})
+
 func main() {
-router := gin.Default()
-
-// Simple group: v1
-v1 := router.Group("/v1")
-{
-v1.POST("/login", loginEndpoint)
-v1.POST("/submit", submitEndpoint)
-v1.POST("/read", readEndpoint)
+	router := gin.Default()
+	router.POST("test", Test)
+	router.Run(":8080")
+}
+func Test(context *gin.Context) {
+	rdb.Set("test", 111, -1)
+	get := rdb.Get("test").String()
+	fmt.Println(get)
+	entry.Info("A group of walrus emerges from the ocean")
 }
 
-// Simple group: v2
-v2 := router.Group("/v2")
-{
-v2.POST("/login", loginEndpoint)
-v2.POST("/submit", submitEndpoint)
-v2.POST("/read", readEndpoint)
-}
+func init() {
+	rdb = redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{":7000", ":7001", ":7002", ":7003", ":7004", ":7005"},
+	})
+	result, err := rdb.Ping().Result()
+	if err != nil {
 
-router.Run(":8080")
+	}
+	fmt.Println(result)
 }
